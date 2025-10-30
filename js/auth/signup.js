2f9ae50332ca18b5efd5d22ee2210d3812914d57
AzErNateSSB <victorp.radio@gmail.com>
2025-10-30 11:24:07 +0100

    25.10.30b QA Commit

--- js/auth/signup.js
+++ js/auth/signup.js
@@ -13,11 +13,12 @@ inputValidationPassword.addEventListener("keyup", validateForm);
 
 //Function permettant de valider tout le formulaire
 function validateForm(){
-    const nomOK = validateRequired(inputNom);
-    const prenomOK = validateRequired(inputPreNom);
-    const mailOK = validateRequired(inputMail);
+    const nomOk = validateRequired(inputNom);
+    const prenomOk = validateRequired(inputPreNom);
+    const mailOk = validateRequired(inputMail);
+    const passwordOk = validateRequired(inputPassword);
 
-    if(nomOK && prenomOK && mailOK){
+    if(nomOk && prenomOk && mailOk && passwordOk){
         btnValidation.disabled = false;
     }
     else{
@@ -30,6 +31,22 @@ function validateMail(input){
     const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
     const mailUser = input.value;
     if(mailUser.match(emailRegex)){
+        input.classList.add("is-valid");
+        input.classList.remove("is-invalid"); 
+        return true;
+    }
+    else{
+        input.classList.remove("is-valid");
+        input.classList.add("is-invalid");
+        return false;
+    }
+}
+
+function validatePassword(input){
+    //Définir mon regex
+    const passwordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[\W_])[A-Za-z\d\W_]{8,}$/;
+    const passwordUser = input.value;
+    if(passwordUser.match(passwordRegex)){
         input.classList.add("is-valid");
         input.classList.remove("is-invalid"); 
         return true;
